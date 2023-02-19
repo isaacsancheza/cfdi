90c849b04a5bccbe905125e4523fcdb140178321
Isaac Sánchez <isaacsancheza@outlook.com>
2023-02-18 19:15:16 -0600

    Fix namespace not found

--- cfdi/complemento/__init__.py
+++ cfdi/complemento/__init__.py
@@ -4,6 +4,13 @@ from .timbre_fiscal_digital import TimbreFiscalDigital
 class Complemento:
     def __init__(self, node):
         self._node = node
+
+        # extract namespaces
+        children = list(node)
+        namespaces = dict()
+        for child in children:
+            namespaces.update(child.nsmap)
+
         self.timbre_fiscal_digital = TimbreFiscalDigital(
-            self._node.find('./{%s}TimbreFiscalDigital' % self._node.nsmap['tfd'])
+            self._node.find('./{%s}TimbreFiscalDigital' % namespaces['tfd'])
         )
